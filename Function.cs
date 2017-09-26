0f682f5063ace1d492afff23e218a9e02dbdfd53
Greater Sum Team <dev01@greatersum.com>
2017-09-26 11:03:29 -0400

    initital commit

---
+++ Function.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Amazon.Lambda.Core;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+// test comment
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace AWSLambda1
+{
+    public class Function
+    {
+
+        private static AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        static string tableName = "myDynamoDB";
+
+        public async Task<Document> FunctionHandler()
+        {
+            Table myDynamoTable = Table.LoadTable(client, "myDynamoTable");
+            Document myDocument = await myDynamoTable.GetItemAsync(1);
+            return myDocument;
+
+        }
+    }
+}
